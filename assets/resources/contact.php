a3688b74dc12b47fc8b9cda5510ca9d312b51e3c
Matt Trueblood <matt@matthewtrueblood.com>
2014-08-11 15:21:58 -0400

    Updated contact logic

--- assets/resources/contact.php
+++ assets/resources/contact.php
@@ -1,6 +1,7 @@
 <?
 require_once('lib/functions.php');
 require_once('lib/swift/swift_required.php');
+
 $name = mysql_real_escape_string($_POST['name']);
 $email = mysql_real_escape_string($_POST['email']);
 $message = mysql_real_escape_string($_POST['message']);
