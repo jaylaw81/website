68b9ffadaff820640b89965ab8725d4297cd3ec1
Jason Law <jlaw@sapient.com>
2014-09-05 16:01:11 -0400

    resources

--- assets/resources/contact.php
+++ assets/resources/contact.php
@@ -1,12 +1,13 @@
 <?php
-    require_once('/assets/resources/lib/functions.php');
-    require_once('/assets/resources/lib/swift/swift_required.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/functions.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/swift/swift_required.php');
     $name = mysql_real_escape_string($_POST['name']);
     $email = mysql_real_escape_string($_POST['email']);
     $message = mysql_real_escape_string($_POST['message']);
     $info = array(
         'name' => $name,
         'email' => $email,
-        'message' => $message);
+        'message' => $message
+    );
     send_contact($info);
 ?>
