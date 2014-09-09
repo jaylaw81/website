5210aa831110a8821590a5a83bb80b9707348c4d
Matt Trueblood <matt@matthewtrueblood.com>
2014-09-09 12:15:22 -0400

    Updated contact.php file

--- assets/resources/contact.php
+++ assets/resources/contact.php
@@ -2,17 +2,13 @@
     include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/config.php');
     include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/functions.php');
     include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/swift/swift_required.php');
-
     $name = mysql_real_escape_string($_POST['name']);
     $email = mysql_real_escape_string($_POST['email']);
     $mess = mysql_real_escape_string($_POST['mess']);
-
-    echo "$name, $email, $mess";
-
-    // $info = array(
-    //     'name' => $name,
-    //     'email' => $email,
-    //     'mess' => $mess
-    // );
-    // send_contact($info);
+    $info = array(
+        'name' => $name,
+        'email' => $email,
+        'mess' => $mess
+    );
+    send_contact($info);
 ?>
