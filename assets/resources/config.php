70b6e960fa9f78ea2dd91085a6b177d8b749119e
Matt Trueblood <matt@matthewtrueblood.com>
2014-09-09 11:58:25 -0400

    Updated config.php file

--- assets/resources/config.php
+++ assets/resources/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $dbname = "";
-$user = "";
-$pass = "";
+$user = "root";
+$pass = "nitrobham2014mysql";
 $conn = mysql_connect($host, $user, $pass) or die("MYSQL ERROR");
 $select_db = mysql_select_db($dbname) or die("Could not select db!");
 ?>
