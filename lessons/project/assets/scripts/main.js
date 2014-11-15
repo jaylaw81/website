321e2be4b9ad77466a22523d2f2aab509b722960
oliver-ben <boliver@sapient.com>
2014-11-14 19:28:45 -0500

    Updated example with full image container

--- lessons/project/assets/scripts/main.js
+++ lessons/project/assets/scripts/main.js
@@ -1,1 +1,9 @@
 
+
+function showImage (argument) {
+
+	var img = "<img src='../assets/images/gallery/large/"+argument+"'>";
+	document.querySelector(".gallery-main-image .image-container").innerHTML = img;
+
+	return false;
+}
