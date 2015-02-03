089b32352b85b8513070315fedfce6e3c9248d65
Matt Trueblood <matt@matthewtrueblood.com>
2015-02-03 13:48:21 -0500

    updated validations on contact form

--- assets/resources/contact.php
+++ assets/resources/contact.php
@@ -1,14 +1,30 @@
 <?php
-    include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/config.php');
-    include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/functions.php');
-    include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/swift/swift_required.php');
-    $name = mysql_real_escape_string($_POST['name']);
-    $email = mysql_real_escape_string($_POST['email']);
-    $mess = mysql_real_escape_string($_POST['mess']);
+include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/config.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/functions.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/assets/resources/lib/swift/swift_required.php');
+$name = mysql_real_escape_string($_POST['name']);
+if(empty($name)) {
+    header("location: ../../contact-rhinocoders/?upd=f")
+}
+$email = mysql_real_escape_string($_POST['email']);
+if(empty($email)) {
+    header("location: ../../contact-rhinocoders/?upd=f")
+}
+$mess = mysql_real_escape_string($_POST['mess']);
+if(empty($mess)) {
+    header("location: ../../contact-rhinocoders/?upd=f")
+}
+$valid = mysql_real_escape_string($_POST['valid']);
+if(empty($valid)) {
+    header("location: ../../contact-rhinocoders/?upd=f")
+} elseif($valid != '5') {
+    header("location: ../../contact-rhinocoders/?upd=f")
+} else {
     $info = array(
         'name' => $name,
         'email' => $email,
         'mess' => $mess
     );
     send_contact($info);
+}
 ?>
